2b1325a14e759542f9e7471094e7beee1c31846e
MauVM <mau@ipor.io>
2024-09-26 07:32:22 +0000

    removed ":" from file names when saving/loading backtest data

--- dope/backengine/maestro.py
+++ dope/backengine/maestro.py
@@ -1,15 +1,14 @@
-import pathlib
 import warnings
 import itertools
 import numpy as np
 import pandas as pd
 from enum import Enum
 import matplotlib.pyplot as plt
-from dataclasses import dataclass
 from collections import defaultdict
 
 from dope.market_impact.linear import LinearMktImpactModel
 from dope.market_impact.neighborhood import NeighborhoodMktImpactModel
+from dope.backengine.backtestdata import BacktestData
 
 
 class Chains(str, Enum):
@@ -77,174 +76,6 @@ class PriceData:
                     break
 
         self.price_dict = price_dict
-
-
-class BacktestData:
-
-    def __init__(self, token_mkt_data: dict[str, dict[str, pd.DataFrame]]):
-        self.token_mkt_data = token_mkt_data
-        self._as_block = {}
-
-    def reset_as_block(self):
-        self._as_block = {}
-
-    def __getitem__(self, key):
-        return self.token_mkt_data[key]
-
-    def keys(self):
-        return self.token_mkt_data.keys()
-
-    def values(self):
-        return self.token_mkt_data.values()
-
-    def get_markets(self):
-        markets = set()
-        for token in self.token_mkt_data.keys():
-            markets.update(self.token_mkt_data[token].keys())
-        return markets
-
-    def items(self):
-        return self.token_mkt_data.items()
-
-    def as_block(self, token):
-        if token not in self._as_block:
-            warnings.simplefilter(action="ignore", category=FutureWarning)
-            self._as_block[token] = pd.concat(
-                self.token_mkt_data[token], names=["datetime"]
-            ).unstack(level=0)
-            warnings.simplefilter("default")
-        return self._as_block[token]
-
-    def to_block(self, token):
-        return self.as_block(token)
-
-    def convert_tvl_from_usd(self, token_price_in_usd_timeseries, inplace=False):
-        this = self if inplace else self.copy()
-
-        price_index = token_price_in_usd_timeseries.index
-        for token in this.token_mkt_data.keys():
-            for mkt in this.token_mkt_data[token].keys():
-                mkt_index = this.token_mkt_data[token][mkt].index
-                # this.token_mkt_data[token][mkt] = this.token_mkt_data[token][mkt][mkt_index.isin(price_index)].copy()
-                mkt_price = token_price_in_usd_timeseries[
-                    token_price_in_usd_timeseries.index.isin(mkt_index)
-                ].copy()
-                for c in ["totalSupplyUsd", "totalBorrowUsd"]:
-                    this.token_mkt_data[token][mkt][c] = (
-                        this.token_mkt_data[token][mkt][c] / mkt_price.price
-                    )
-
-        this.reset_as_block()
-
-        return this
-
-    def copy(self):
-        copy_token_mkt_data = {}
-        for token, data in self.token_mkt_data.items():
-            copy_token_mkt_data[token] = {}
-            for mkt, df in data.items():
-                copy_token_mkt_data[token][mkt] = df.copy()
-        return BacktestData(copy_token_mkt_data)
-
-    def get_dates(self):
-        dates = set()
-        for token, data in self.token_mkt_data.items():
-            for protocol, df in data.items():
-                dates.update(df.index)
-        dates = list(dates)
-        dates.sort()
-        return dates
-
-    def apply_whitelist(self, whitelist):
-        """
-        Usage example:
-
-        whitelist = {
-          "USDC": [
-            #"makerdao:USDC",
-            "aave-v3:USDC",
-            "compound-v3:USDC",
-            "aave-v2:USDC",
-            "compound-v2:USDC"
-            ],
-          "wBTC": [
-            "aave-v3:WBTC",
-            "aave-v2:WBTC",
-            "compound-v3:WBTC",
-            "compound-v2:WBTC",
-            #"makerdao:WBTC"
-          ],
-          "USDT": [
-            #"makerdao:USDC",
-            "aave-v3:USDT",
-            "compound-v3:USDT",
-            "aave-v2:USDT",
-            "compound-v2:USDT"
-            ],
-          "DAI": [
-            #"makerdao:USDC",
-            "aave-v3:DAI",
-            "compound-v3:DAI",
-            "aave-v2:DAI",
-            "compound-v2:DAI"
-            ],
-        }
-        """
-
-        for t in self.token_mkt_data.keys():
-            if t in whitelist:
-                to_delete = [
-                    k for k in self.token_mkt_data[t].keys() if k not in whitelist[t]
-                ]
-                for k in to_delete:
-                    del self.token_mkt_data[t][k]
-
-    def apply_blacklist(self, blacklist):
-        for t in self.token_mkt_data.keys():
-            if t in blacklist:
-                to_delete = [
-                    k for k in self.token_mkt_data[t].keys() if k in blacklist[t]
-                ]
-                for k in to_delete:
-                    del self.token_mkt_data[t][k]
-
-    def add_cash_mkt(self):
-        for token in self.token_mkt_data.keys():
-            mkts = list(self.token_mkt_data[token].keys())
-            if len(mkts) == 0:
-                return
-            mkt_example = self.token_mkt_data[token][mkts[0]]
-            self.token_mkt_data[token]["cash"] = mkt_example.copy(deep=True)
-            self.token_mkt_data[token]["cash"]["apyBase"] = 0
-            self.token_mkt_data[token]["cash"]["apyBaseBorrow"] = np.inf
-
-    def dump(
-        self, filename, folderpath=pathlib.Path().home() / "s3/fusion/backtest-data"
-    ):
-        pathlib.Path(folderpath / filename).mkdir(parents=True, exist_ok=True)
-        # delete data already in folder to avoid overlapping old and new data:
-        for file in (folderpath / filename).iterdir():
-            if file.is_file():
-                file.unlink()
-
-        for token, data in self.token_mkt_data.items():
-            for mkt, df in data.items():
-                df.to_csv(folderpath / filename / f"{token}_{mkt}.csv")
-
-    @classmethod
-    def load(
-        cls, filename, folderpath=pathlib.Path().home() / "s3/fusion/backtest-data"
-    ):
-        token_mkt_data = {}
-        for file in pathlib.Path(folderpath / filename).iterdir():
-            token, mkt = file.stem.split("_")
-            if token not in token_mkt_data:
-                token_mkt_data[token] = {}
-            token_mkt_data[token][mkt] = pd.read_csv(file, index_col=0)
-            token_mkt_data[token][mkt].index = pd.to_datetime(
-                token_mkt_data[token][mkt].index
-            )
-        return cls(token_mkt_data)
 
 
 class DataLoader:
--- dope/names/poolname.py
+++ dope/names/poolname.py
@@ -21,11 +21,28 @@ class PoolName:
             if not attr.startswith("__") and not callable(getattr(self, attr)):
                 yield getattr(self, attr)
     
+    @classmethod
+    def from_str(cls, string):
+        first, last = string.find(":"), string[::-1].find(":")
+        return PoolName(string[:first], string[first+1:-last-1], string[-last:])
+    
+    def __eq__(self, other):
+        if isinstance(other, PoolName):
+            return (self.chain, self.protocol, self.token_name) == (other.chain, other.protocol, other.token_name)
+        elif isinstance(other, str):
+            return repr(self) == other
+        return False
+    
     def to_disk_name(self):
         return f"chain___{self.chain}___protocol___{self.protocol}___token___{self.token_name}"
     
     @classmethod
-    def from_disk_name(cls, disk_name):
-        disk_name = disk_name.split("___")
-        return cls(chain=disk_name[1], protocol=disk_name[3], token_name=disk_name[5])
+    def try_from_disk_name(cls, disk_name, verbose=True):
+        try:
+            name_lst = disk_name.split("___")
+            return cls(chain=name_lst[1], protocol=name_lst[3], token_name=name_lst[5])
+        except Exception as e:
+            if verbose:
+                print(f"Warning: {e} when loading {disk_name}.")
+            return disk_name
     
